e43a0efee4c3cc5fcb5d6a388a8278b9c8bad65f
dymyw <dymayongwei@163.com>
2020-05-18 19:30:14 +0800

    feature: add confirm publish

--- src/Amqp/Producer.php
+++ src/Amqp/Producer.php
@@ -22,4 +22,41 @@ class Producer extends AbstractProducer
             $this->routingKey
         );
     }
+
+    /**
+     * @param string $messageBody
+     * @param int $timeout
+     */
+    public function confirmPublish($messageBody, $timeout = 1)
+    {
+        $message = $this->getMessage($messageBody);
+
+        // confirm mode
+        $this->channel->confirm_select();
+
+        $this->channel->basic_publish(
+            $message,
+            $this->exchangeOptions['name'],
+            $this->routingKey
+        );
+
+        // wait
+        $this->channel->wait_for_pending_acks($timeout);
+    }
+
+    /**
+     * @param callable $callback
+     */
+    public function setAckHandler(callable $callback)
+    {
+        $this->channel->set_ack_handler($callback);
+    }
+
+    /**
+     * @param callable $callback
+     */
+    public function setNackHandler(callable $callback)
+    {
+        $this->channel->set_nack_handler($callback);
+    }
 }
