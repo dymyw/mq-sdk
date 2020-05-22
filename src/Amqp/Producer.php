24a2d3f961d089d0c8ca37c4e668d638cd328036
dymyw <dymayongwei@163.com>
2020-05-22 09:04:40 +0800

    feature: batch send message

--- src/Amqp/Producer.php
+++ src/Amqp/Producer.php
@@ -2,6 +2,8 @@
 
 namespace Mq\Amqp;
 
+use InvalidArgumentException;
+
 /**
  * Class Producer
  * @package Mq\Amqp
@@ -21,5 +23,28 @@ class Producer extends AbstractProducer
             $this->exchangeOptions['name'],
             $this->routingKey
         );
+    }
+
+    /**
+     * @param array $messageBodies
+     */
+    public function batchPublish(array $messageBodies)
+    {
+        foreach ($messageBodies as $key => $messageBody) {
+            if (!is_string($messageBody) && !is_int($messageBody)) {
+                throw new InvalidArgumentException(
+                    'Message body must be string or integer, index: ' . $key
+                );
+            }
+            $message = $this->getMessage($messageBody);
+
+            $this->channel->batch_basic_publish(
+                $message,
+                $this->exchangeOptions['name'],
+                $this->routingKey
+            );
+        }
+
+        $this->channel->publish_batch();
     }
 }
